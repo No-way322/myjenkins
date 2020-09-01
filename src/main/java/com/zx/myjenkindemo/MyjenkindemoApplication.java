8914ee79762c0091adbc5ba203994293c7a8bfdf
zhengxiang <916179760@qq.com>
2020-09-01 17:51:02 +0800

    first commit

---
+++ src/main/java/com/zx/myjenkindemo/MyjenkindemoApplication.java
@@ -0,0 +1,13 @@
+package com.zx.myjenkindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjenkindemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyjenkindemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zx/myjenkindemo/MyjenkindemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zx.myjenkindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyjenkindemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
