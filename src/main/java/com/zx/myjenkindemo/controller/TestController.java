9bf6c4e5eee10153bf34c96f4a5acbb2959e82d0
zhengxiang <916179760@qq.com>
2020-09-02 15:29:22 +0800

    test

--- src/main/java/com/zx/myjenkindemo/controller/TestController.java
+++ src/main/java/com/zx/myjenkindemo/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @RequestMapping("test")
     public String test(){
-        return "hello my first jenkinsdemo";
+        return "zzzzzzzhello my first jenkinsdemo";
     }
 }
